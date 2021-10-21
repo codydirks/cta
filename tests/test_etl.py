2a922f62450268131e4f694e5526fd8bbb32eaa4
Cody Dirks <cody.dirks@strong.io>
2021-10-21 12:02:47 -0500

    Update tests.

--- tests/test_etl.py
+++ tests/test_etl.py
@@ -1,6 +1,26 @@
 import unittest
+from unittest.mock import patch
 
-from cta.etl import fetch_daily_ridership_data, fetch_station_info
+from cta.etl import (
+    fetch_daily_ridership_data, fetch_station_info,
+    load_raw_station_info, load_raw_riders_df, load_processed_df
+)
+
+expected_ridership_cols = ['station_id', 'stationname', 'date', 'daytype', 'rides']
+
+expected_station_info_cols = [
+    'stop_id',
+    'direction_id',
+    'stop_name',
+    'station_name',
+    'station_descriptive_name',
+    'map_id',
+    'ada',
+    'red', 'blue', 'g', 'brn', 'p', 'pexp', 'y', 'pnk', 'o',
+    'location'
+]
+
+expected_processed_dataset_cols = ['dataset', 'date', 'rides', 'line', 'daytype']
 
 
 class TestEtl(unittest.TestCase):
@@ -9,11 +29,10 @@ class TestEtl(unittest.TestCase):
         for limit in limits:
             df = fetch_daily_ridership_data(limit=limit)
             assert len(df) == limit
-            expected_cols = ['station_id', 'stationname', 'date', 'daytype', 'rides']
-            extra_cols = set(df.columns) - set(expected_cols)
-            missing_cols = set(expected_cols) - set(df.columns)
+            extra_cols = set(df.columns) - set(expected_ridership_cols)
+            missing_cols = set(expected_ridership_cols) - set(df.columns)
             assert extra_cols == set(), f"Limit: {limit}, Extra columns: {extra_cols}"
-            assert missing_cols == set(), f"Limit: {limit}, Extra columns: {missing_cols}"
+            assert missing_cols == set(), f"Limit: {limit}, Missing columns: {missing_cols}"
 
     def test_fetch_station_info(self):
         limits = [1, 10, 100]
@@ -21,11 +40,48 @@ class TestEtl(unittest.TestCase):
             df = fetch_station_info(limit=limit)
             assert len(df) == limit
 
-            expected_cols = [
-                'stop_id', 'direction_id', 'stop_name', 'station_name', 'station_descriptive_name', 'map_id',
-                'ada', 'red', 'blue', 'g', 'brn', 'p', 'pexp', 'y', 'pnk', 'o', 'location'
-            ]
-            extra_cols = set(df.columns) - set(expected_cols)
-            missing_cols = set(expected_cols) - set(df.columns)
+            extra_cols = set(df.columns) - set(expected_station_info_cols)
+            missing_cols = set(expected_station_info_cols) - set(df.columns)
             assert extra_cols == set(), f"Limit: {limit}, Extra columns: {extra_cols}"
-            assert missing_cols == set(), f"Limit: {limit}, Extra columns: {missing_cols}"
+            assert missing_cols == set(), f"Limit: {limit}, Missing columns: {missing_cols}"
+
+    def path_patcher(*join_args):
+        this_dir = __file__
+        fl = join_args[-1]
+        return this_dir.replace("test_etl.py", f"sample_{fl}")
+
+    @patch("cta.etl.os.path.join")
+    def test_load_raw_station_info(self, mock_path_join):
+        mock_path_join.side_effect = self.path_patcher
+        df = load_raw_station_info()
+
+        cols = df.columns.str.lower()
+        extra_cols = set(cols) - set(expected_station_info_cols)
+        missing_cols = set(expected_station_info_cols) - set(cols)
+
+        assert extra_cols == set(), f"Extra columns: {extra_cols}"
+        assert missing_cols == set(), f"Missing columns: {missing_cols}"
+
+    @patch("cta.etl.os.path.join")
+    def test_load_raw_riders_df(self, mock_path_join):
+        mock_path_join.side_effect = self.path_patcher
+        df = load_raw_riders_df()
+
+        cols = df.columns.str.lower()
+        extra_cols = set(cols) - set(expected_ridership_cols)
+        missing_cols = set(expected_ridership_cols) - set(cols)
+
+        assert extra_cols == set(), f"Extra columns: {extra_cols}"
+        assert missing_cols == set(), f"Missing columns: {missing_cols}"
+
+    @patch("cta.etl.os.path.join")
+    def test_load_processed_df(self, mock_path_join):
+        mock_path_join.side_effect = self.path_patcher
+        df = load_processed_df()
+
+        cols = df.columns.str.lower()
+        extra_cols = set(cols) - set(expected_processed_dataset_cols)
+        missing_cols = set(expected_processed_dataset_cols) - set(cols)
+
+        assert extra_cols == set(), f"Extra columns: {extra_cols}"
+        assert missing_cols == set(), f"Missing columns: {missing_cols}"
